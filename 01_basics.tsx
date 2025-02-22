5a7d44f29ba264008a33c01070a3ac7138dd84c6
Harshit kumar yadav <harshitkmryadav@gmail.com>
2025-02-22 18:05:02 +0530

    Initial commit

---
+++ 01_basics.tsx
@@ -0,0 +1,5 @@
+// Typescript is more about type Safety while writing the script.
+// Static Checking 
+// Typescript is kinda development tool you main code still runs on Javascript
+//in javascript   2+"2" = '22' , null+2 = 2, undefined+2 = NaN
+
